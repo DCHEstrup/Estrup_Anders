944456258b41364c2a81ae9943fa001d503fb57d
Dansk Center for Herregårdsforskning <dch@gammelestrup.dk>
2026-09-02 09:30:34 +0200

    Remove outdated comments and clean up Gruntfile

--- htck/Gruntfile.js
+++ htck/Gruntfile.js
@@ -424,7 +424,6 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-protractor-runner');
 
 
   grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
